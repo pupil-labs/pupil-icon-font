90af7bcce79e482b416bddd0354adc321e147633
nathakits <nathakit.sae-tan@cuinda.com>
2017-08-25 18:26:33 +0700

    update svg py script to update new svgs with correct size

--- svg.py
+++ svg.py
@@ -4,20 +4,20 @@ import glob
 
 # modify width and height as desired
 
-width = "24"
-height = "24"
+width = "1000"
+height = "1000"
 
-scale = 0.7
-vb = 24
+# scale = 0.7
+# vb = 1000
 
-x = vb * scale
-y = vb - x
-z = y / 2
+# x = vb * scale
+# y = vb - x
+# z = y / 2
 
-trans = str(z)
+# trans = str(z)
 
 # put all your svgs in a dir called svgs or modify the path accordingly
-svg_file_paths = glob.glob('./svgs/*.svg')
+svg_file_paths = glob.glob('./icons_1000x/*.svg')
 
 for p in svg_file_paths:
     fp = os.path.abspath(p)
@@ -28,13 +28,13 @@ for p in svg_file_paths:
     svg.setAttribute('width', width)
     svg.setAttribute('height', height)
 
-    rect = doc.getElementsByTagName('rect')[0]
-    rect.attributes['width'].value = str(vb)
-    rect.attributes['height'].value = str(vb)
+    # rect = doc.getElementsByTagName('rect')[0]
+    # rect.attributes['width'].value = str(vb)
+    # rect.attributes['height'].value = str(vb)
 
-    path = doc.getElementsByTagName('path')[0]
-    path.setAttribute('transform',
-                        "translate({0} {0})" " scale({1})".format(trans,scale))
+    # path = doc.getElementsByTagName('path')[0]
+    # path.setAttribute('transform',
+    #                     "translate({0} {0})" " scale({1})".format(trans,scale))
 
     with open(fp,'w') as f:
         doc.firstChild.writexml(f)
